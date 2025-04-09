926d7f7320312c80eb51d9b44f2b4b2c0c907b59
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-09 23:38:21 +0500

    VillaAPI controller added.

---
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+	}
+}
