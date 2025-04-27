0504493e2186a4d54a56d618904e2cc46d8c669f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-27 23:51:21 +0500

    Removed hard coded statuses.

--- MagicVilla.API/Controllers/VillaAPIController.cs
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -18,9 +18,12 @@ namespace MagicVilla.API.Controllers
 
 		[HttpGet("{id}")]
 		//[ProducesResponseType(200)]
-		[ProducesResponseType(200, Type = typeof(VillaDTO))]
-		[ProducesResponseType(400)]
-		[ProducesResponseType(404)]
+		//[ProducesResponseType(200, Type = typeof(VillaDTO))]
+		//[ProducesResponseType(400)]
+		//[ProducesResponseType(404)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public ActionResult GetVilla(int id)
 		{
 			if (id == 0)
