6ecb04deaf7df80536ecb8f4664a46ff87b19123
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-05-10 23:33:34 +0500

    New fields values updated in Store.

--- MagicVilla.API/Data/VillaStore.cs
+++ MagicVilla.API/Data/VillaStore.cs
@@ -7,8 +7,8 @@ namespace MagicVilla.API.Data
 	{
 		public static List<VillaDTO> VillaList = new List<VillaDTO>()
 		{
-			new VillaDTO { Id = 1, Name = "Pool Villa" },
-			new VillaDTO { Id = 2, Name = "Pool Villa" }
+			new VillaDTO { Id = 1, Name = "Pool Villa", Occupancy = 100, SqFt = 10000 },
+			new VillaDTO { Id = 2, Name = "Pool Villa", Occupancy = 250, SqFt = 20000 }
 		};
 	}
 }
