42380e9f6faaf761ffdc14bbe5fae6cbfa010c82
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-11 22:34:05 +0500

    Serilog configuration added.

--- MagicVilla.API/Program.cs
+++ MagicVilla.API/Program.cs
@@ -1,6 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("log/logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers(options =>
 {
