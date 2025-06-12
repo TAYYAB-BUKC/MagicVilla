78d846b7358452b670521f1a92dbd9711a0aca29
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-12 22:53:00 +0500

    Custom logging implementation added.

---
+++ MagicVilla.API/Logging/ILogging.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla.API.Logging
+{
+	public interface ILogging
+	{
+		void Log(string message, string type);
+	}
+}
---
+++ MagicVilla.API/Logging/Logging.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla.API.Logging
+{
+	public class Logging : ILogging
+	{
+		public void Log(string message, string type)
+		{
+			Console.WriteLine($"{type.ToUpper()} - {message}");
+		}
+	}
+}
