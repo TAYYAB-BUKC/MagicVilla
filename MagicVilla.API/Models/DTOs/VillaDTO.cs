bbc3ccba89cdfe6ec705cb892e019cc5c14a750f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-13 16:37:22 +0500

    Villa and VillaDTO updated.

--- MagicVilla.API/Models/DTOs/VillaDTO.cs
+++ MagicVilla.API/Models/DTOs/VillaDTO.cs
@@ -10,5 +10,10 @@ namespace MagicVilla.API.Models.DTOs
 		public string? Name { get; set; }
 		public int SqFt { get; set; }
 		public int Occupancy { get; set; }
+		public string? Details { get; set; }
+		[Required]
+		public double Rate { get; set; }
+		public string? ImageURL { get; set; }
+		public string? Amenity { get; set; }
 	}
 }
