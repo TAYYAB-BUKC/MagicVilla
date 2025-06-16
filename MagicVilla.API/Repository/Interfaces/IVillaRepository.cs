a28848e663f6de0b544571e2ae48087ac695a329
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-16 07:26:58 +0500

    IVillaRepository added.

---
+++ MagicVilla.API/Repository/Interfaces/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla.API.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla.API.Repository.Interfaces
+{
+	public interface IVillaRepository
+	{
+		Task<List<Villa>> GetAll(Expression<Func<Villa>> filters = null!);
+		Task<Villa> GetById(Expression<Func<Villa>> filters = null!, bool tracked = true);
+		Task Create(Villa entity);
+		Task Remove(Villa entity);
+		Task Save();
+	}
+}
