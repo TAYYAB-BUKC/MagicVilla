cf4d027368828afb7115f648616fc5fc469f49a5
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-16 11:48:54 +0500

    VillaNumberRepository interface and implementation added.

---
+++ MagicVilla.API/Repository/Interfaces/IVillaNumberRepository.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla.API.Models;
+
+namespace MagicVilla.API.Repository.Interfaces
+{
+	public interface IVillaNumberRepository : IGenericRepository<VillaNumber>
+	{
+		Task<VillaNumber> UpdateAsync(VillaNumber entity);
+	}
+}
---
+++ MagicVilla.API/Repository/VillaNumberRepository.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla.API.Data;
+using MagicVilla.API.Models;
+using MagicVilla.API.Repository.Interfaces;
+
+namespace MagicVilla.API.Repository
+{
+	public class VillaNumberRepository : GenericRepository<VillaNumber>, IVillaNumberRepository
+	{
+		public readonly ApplicationDbContext _dbContext;
+		public VillaNumberRepository(ApplicationDbContext dbContext) : base(dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<VillaNumber> UpdateAsync(VillaNumber entity)
+		{
+			_dbContext.VillaNumbers.Update(entity);
+			await _dbContext.SaveChangesAsync();
+			return entity;
+		}
+	}
+}
