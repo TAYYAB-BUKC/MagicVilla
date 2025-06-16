b91eaab58be0a346c21e4d667d6057b7da5598e3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-16 12:23:27 +0500

    VillaID added in VillaNumber model and DTOs.

--- MagicVilla.API/Models/DTOs/VillaNumberCreateDTO.cs
+++ MagicVilla.API/Models/DTOs/VillaNumberCreateDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVilla.API.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.API.Models.DTOs
 {
 	public class VillaNumberCreateDTO
 	{
+		[Required]
 		public int VillaNo { get; set; }
+		[Required]
+		public int VillaID { get; set; }
 		public string? SpecialDetails { get; set; }
 	}
 }
--- MagicVilla.API/Models/DTOs/VillaNumberDTO.cs
+++ MagicVilla.API/Models/DTOs/VillaNumberDTO.cs
@@ -3,6 +3,7 @@
 	public class VillaNumberDTO
 	{
 		public int VillaNo { get; set; }
+		public int VillaID { get; set; }
 		public string? SpecialDetails { get; set; }
 	}
 }
