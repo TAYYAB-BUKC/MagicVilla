270061788f41afeba7e8ea6187bb895e01cca831
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-19 11:08:35 +0500

    Request model added in web project.

---
+++ MagicVilla.Web/Models/Request.cs
@@ -0,0 +1,11 @@
+﻿using static MagicVilla.Utility.Configuration;
+
+namespace MagicVilla.Web.Models
+{
+	public class Request
+	{
+		public RequestType RequestType { get; set; }
+		public required string URL { get; set; }
+		public object? Data { get; set; }
+	}
+}
