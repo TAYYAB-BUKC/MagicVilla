962cde44e9236a0346dbabf255daba9903a89c20
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-19 11:12:26 +0500

    AutoMapper mappings configuration added.

---
+++ MagicVilla.Web/Mappings/MappingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla.Web.Models.DTOs;
+
+namespace MagicVilla.Web.Mappings
+{
+	public class MappingConfiguration : Profile
+	{
+		public MappingConfiguration()
+		{
+			CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+			CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+			
+			CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+			CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+		}
+	}
+}
