fe072ccaed98abdf49bdb5b2defcb9c12a6fc3d9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-19 14:35:47 +0500

    VillaNumberService implementation added.

---
+++ MagicVilla.Web/Services/IServices/IVillaNumberService.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla.Web.Models.DTOs;
+
+namespace MagicVilla.Web.Services.IServices
+{
+	public interface IVillaNumberService
+	{
+		Task<T> GetAllAsync<T>();
+		Task<T> GetAsync<T>(int id);
+		Task<T> CreateAsync<T>(VillaNumberCreateDTO villa);
+		Task<T> UpdateAsync<T>(VillaNumberUpdateDTO villa);
+		Task<T> DeleteAsync<T>(int id);
+	}
+}
---
+++ MagicVilla.Web/Services/VillaNumberService.cs
@@ -0,0 +1,65 @@
+﻿using MagicVilla.Web.Models;
+using MagicVilla.Web.Models.DTOs;
+using MagicVilla.Web.Services.IServices;
+using static MagicVilla.Utility.Configuration;
+
+namespace MagicVilla.Web.Services
+{
+	public class VillaNumberService : BaseService, IVillaNumberService
+	{
+		public IHttpClientFactory httpclient { get; set; }
+		public string? BASE_URL { get; set; }
+		public VillaNumberService(IHttpClientFactory httpclient, IConfiguration configuration) : base(httpclient)
+		{
+			this.httpclient = httpclient;
+			this.BASE_URL = configuration.GetValue<string>("ServiceURLs:VillaAPI");
+		}
+
+		public async Task<T> GetAllAsync<T>()
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.GET,
+				URL = $"{BASE_URL}/api/VillaNumberAPI"
+			});
+		}
+
+		public async Task<T> GetAsync<T>(int id)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.GET,
+				URL = $"{BASE_URL}/api/VillaNumberAPI/{id}"
+			});
+		}
+
+		public async Task<T> CreateAsync<T>(VillaNumberCreateDTO villa)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.POST,
+				URL = $"{BASE_URL}/api/VillaNumberAPI",
+				Data = villa
+			});
+		}
+
+		public async Task<T> UpdateAsync<T>(VillaNumberUpdateDTO villa)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.PUT,
+				URL = $"{BASE_URL}/api/VillaNumberAPI/{villa.VillaID}",
+				Data = villa
+			});
+		}
+
+		public async Task<T> DeleteAsync<T>(int id)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.DELETE,
+				URL = $"{BASE_URL}/api/VillaNumberAPI/{id}",
+			});
+		}
+	}
+}
