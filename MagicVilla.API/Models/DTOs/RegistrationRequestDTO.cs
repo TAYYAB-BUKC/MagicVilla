518b8fabf0536304f7124b00fcceeb5f002e18a7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-20 23:13:11 +0500

    RegistrationRequest DTO added.

---
+++ MagicVilla.API/Models/DTOs/RegistrationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla.API.Models.DTOs
+{
+	public class RegistrationRequestDTO
+	{
+		public required string Name { get; set; }
+		public required string Username { get; set; }
+		public required string Password { get; set; }
+		public required string Role { get; set; }
+	}
+}
