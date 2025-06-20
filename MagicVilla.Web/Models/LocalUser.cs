5cca3daedb3a61cd62ccb93ddb8f9a5b2e2486bc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-20 23:01:49 +0500

    LocalUser model added.

---
+++ MagicVilla.Web/Models/LocalUser.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla.Web.Models
+{
+	public class LocalUser
+	{
+		public int Id { get; set; }
+		public required string Name { get; set; }
+		public required string Username { get; set; }
+		public required string Password { get; set; }
+		public required string Role { get; set; }
+	}
+}
