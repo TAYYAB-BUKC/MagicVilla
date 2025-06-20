77b789659bebc9bf5371e8e3d88815d8f9649e17
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-21 02:00:11 +0500

    ErrorMessages null issue resolved.

--- MagicVilla.API/Models/Response.cs
+++ MagicVilla.API/Models/Response.cs
@@ -4,6 +4,10 @@ namespace MagicVilla.API.Models
 {
 	public class Response
 	{
+		public Response()
+		{
+			ErrorMessages = new List<string>();
+		}
 		public HttpStatusCode StatusCode { get; set; }
 		public bool IsSuccess { get; set; }
 		public List<string>? ErrorMessages { get; set; }
