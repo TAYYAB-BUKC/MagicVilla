b9ed8ecb4b7247f285a3481ccf78b2a915bd27d1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-22 10:16:31 +0500

    Session added in pipeline.

--- MagicVilla.Web/Program.cs
+++ MagicVilla.Web/Program.cs
@@ -35,11 +35,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
+app.UseSession();
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
