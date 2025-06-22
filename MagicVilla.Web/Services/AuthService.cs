95e59f91ae1e48f8d8d7157f61c80f1ad76edb1e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-22 09:45:05 +0500

    AuthService implementation added.

---
+++ MagicVilla.Web/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla.Web.Models;
+using MagicVilla.Web.Models.DTOs;
+using MagicVilla.Web.Services.IServices;
+using static MagicVilla.Utility.Configuration;
+
+namespace MagicVilla.Web.Services
+{
+	public class AuthService : BaseService, IAuthService
+	{
+		public IHttpClientFactory httpclient { get; set; }
+		public string? BASE_URL { get; set; }
+		public AuthService(IHttpClientFactory httpclient, IConfiguration configuration) : base(httpclient)
+		{
+			this.httpclient = httpclient;
+			this.BASE_URL = configuration.GetValue<string>("ServiceURLs:VillaAPI");
+		}
+
+		public async Task<T> LoginAsync<T>(LoginRequestDTO requestDTO)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.POST,
+				URL = $"{BASE_URL}/api/UserAuth/login",
+				Data = requestDTO
+			});
+		}
+
+		public async Task<T> RegisterAsync<T>(RegistrationRequestDTO requestDTO)
+		{
+			return await SendAsync<T>(new Request()
+			{
+				RequestType = RequestType.POST,
+				URL = $"{BASE_URL}/api/UserAuth/register",
+				Data = requestDTO
+			});
+		}
+	}
+}
---
+++ MagicVilla.Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla.Web.Models.DTOs;
+
+namespace MagicVilla.Web.Services.IServices
+{
+	public interface IAuthService
+	{
+		Task<T> LoginAsync<T>(LoginRequestDTO requestDTO);
+		Task<T> RegisterAsync<T>(RegistrationRequestDTO requestDTO);
+	}
+}
