0b490eaf9a6fa32a64a5f72f676f74d99840778c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-23 10:06:38 +0500

    Send supported ApiVersions in response headers.

--- MagicVilla.API/Program.cs
+++ MagicVilla.API/Program.cs
@@ -91,6 +91,7 @@ builder.Services.AddAuthentication(options => {
 builder.Services.AddApiVersioning(options => {
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
 });
 
 builder.Services.AddVersionedApiExplorer(options =>
