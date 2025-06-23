ea9f3776cf32f272dce36f248e8978ad82ae618a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-23 14:44:01 +0500

    Filter implemented in v2 API of Villa.

--- MagicVilla.API/Controllers/v2/VillaAPIv2Controller.cs
+++ MagicVilla.API/Controllers/v2/VillaAPIv2Controller.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using MagicVilla.API.Controllers.v1;
+using MagicVilla.API.Logging;
+using MagicVilla.API.Models;
+using MagicVilla.API.Models.DTOs;
+using MagicVilla.API.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using static MagicVilla.Utility.Configuration;
 
 namespace MagicVilla.API.Controllers.v2
 {
@@ -7,6 +16,49 @@ namespace MagicVilla.API.Controllers.v2
 	[ApiVersion("2.0")]
 	public class VillaAPIv2Controller : ControllerBase
 	{
+		private readonly IVillaRepository _villaRepository;
+		private readonly IMapper _mapper;
+		private readonly Response _response;
+
+		public VillaAPIv2Controller(IVillaRepository villaRepository, IMapper mapper)
+		{
+			_villaRepository = villaRepository;
+			_mapper = mapper;
+			_response = new();
+		}
+
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ResponseCache(CacheProfileName = CacheProfileName)]
+		public async Task<ActionResult<Response>> GetVillas([FromQuery(Name = "FilterVillasByOccupany")] int? occupancy)
+		{
+			try
+			{
+				IEnumerable<Villa> villas;
+
+				if (occupancy > 0)
+				{
+					villas = await _villaRepository.GetAllAsync(v => v.Occupancy == occupancy);
+				}
+				else
+				{
+					villas = await _villaRepository.GetAllAsync();
+				}
+
+				var data = _mapper.Map<IEnumerable<Villa>, IEnumerable<VillaDTO>>(villas);
+				_response.StatusCode = HttpStatusCode.OK;
+				_response.IsSuccess = true;
+				_response.Data = data;
+			}
+			catch (Exception ex)
+			{
+				_response.StatusCode = HttpStatusCode.InternalServerError;
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>() { Convert.ToString(ex.Message) };
+			}
+			return _response;
+		}
+
 		[HttpGet("GetString")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		public IEnumerable<string> GetVillasVersion2()
