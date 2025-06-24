45bd8103114994db7ac84f23e4151c9f5d45a350
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-24 22:49:14 +0500

    Configured v2 as the default documentation.

--- MagicVilla.API/Program.cs
+++ MagicVilla.API/Program.cs
@@ -158,8 +158,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(options =>
 	{
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Magic_Villa_V1");
 		options.SwaggerEndpoint("/swagger/v2/swagger.json", "Magic_Villa_V2");
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Magic_Villa_V1");
 	});
 }
 
