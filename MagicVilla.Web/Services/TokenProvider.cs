2b77e57801eddffff81aa78708e9d3b8d396ce69
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-30 09:53:01 +0500

    ITokenProvider implementation added.

---
+++ MagicVilla.Web/Services/TokenProvider.cs
@@ -0,0 +1,33 @@
+﻿using MagicVilla.Web.Models.DTOs;
+using MagicVilla.Web.Services.IServices;
+using static MagicVilla.Utility.Configuration;
+
+namespace MagicVilla.Web.Services
+{
+	public class TokenProvider : ITokenProvider
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public TokenProvider(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public LoginResponseDTO GetToken()
+		{
+			return new LoginResponseDTO()
+			{
+				Token = _httpContextAccessor.HttpContext?.Session.GetString(AccessToken)
+			};
+		}
+
+		public void ClearToken()
+		{
+			_httpContextAccessor.HttpContext?.Session.SetString(AccessToken, string.Empty);
+		}
+
+		public void SetToken(LoginResponseDTO model)
+		{
+			_httpContextAccessor.HttpContext?.Session.SetString(AccessToken, model.Token);
+		}
+	}
+}
