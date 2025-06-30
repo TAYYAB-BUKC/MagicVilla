c4485eba62df7f7541fef316a60083ee5be2b9aa
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-30 09:46:54 +0500

    ITokenProvider added.

---
+++ MagicVilla.Web/Services/IServices/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla.Web.Models.DTOs;
+
+namespace MagicVilla.Web.Services.IServices
+{
+	public interface ITokenProvider
+	{
+		LoginResponseDTO GetToken();
+		void SetToken(LoginResponseDTO model);
+		void ClearToken();
+	}
+}
