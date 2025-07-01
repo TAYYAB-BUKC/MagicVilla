5df8a5051722ec81f1d6c40917be7b39abdb5666
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-01 14:47:59 +0500

    RefreshToken added in ApplicationDbContext.

--- MagicVilla.API/Data/ApplicationDbContext.cs
+++ MagicVilla.API/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace MagicVilla.API.Data
 
 		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+		public DbSet<RefreshToken> RefreshTokens { get; set; }
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
