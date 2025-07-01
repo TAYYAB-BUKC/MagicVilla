85a8c6b8e55652f87613fc58e4db0dd200408c0d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-01 14:47:05 +0500

    RefreshToken model added.

---
+++ MagicVilla.API/Models/RefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla.API.Models
+{
+	public class RefreshToken
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; }
+		public string JWTTokenId { get; set; }
+		public string? JWTRefreshToken { get; set; }
+		public bool IsValid { get; set; }
+		public DateTime ExpiresAt { get; set; }
+	}
+}
