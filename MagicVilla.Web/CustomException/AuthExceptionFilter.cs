efcea2493fabacee6820b8563af51fdd748275b7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-02 15:20:25 +0500

    AuthExceptionFilter implemented.

---
+++ MagicVilla.Web/CustomException/AuthExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MagicVilla.Web.CustomException
+{
+	public class AuthExceptionFilter : IExceptionFilter
+	{
+		public void OnException(ExceptionContext context)
+		{
+			if(context.Exception is AuthException)
+			{
+				context.Result = new RedirectToActionResult("Login", "Auth", null);
+			}
+		}
+	}
+}
