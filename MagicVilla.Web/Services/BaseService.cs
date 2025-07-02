4a4c3701d4c19922284c1b0cbd65138876c9faec
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-02 11:54:19 +0500

    SendAsync implementation enhanced with httpRequestMessageFactory.

--- MagicVilla.Web/Services/BaseService.cs
+++ MagicVilla.Web/Services/BaseService.cs
@@ -26,66 +26,71 @@ namespace MagicVilla.Web.Services
 			try
 			{
 				var client = _httpClient.CreateClient("MagicAPI");
-				HttpRequestMessage httpRequest = new HttpRequestMessage();
-				if (request.ContentType == ContentType.MultipartFormData)
+				var httpRequestMessageFactory = () =>
 				{
-					httpRequest.Headers.Add("Accept", "*/*");
-				}
-				else
-				{
-					httpRequest.Headers.Add("Accept", "application/json");
-				}
-				httpRequest.RequestUri = new Uri(request.URL);
-
-				if (request.ContentType == ContentType.MultipartFormData)
-				{
-					var content = new MultipartFormDataContent();
-
-					foreach (var prop in request.Data.GetType().GetProperties())
+					HttpRequestMessage httpRequest = new();
+					if (request.ContentType == ContentType.MultipartFormData)
 					{
-						var value = prop.GetValue(request.Data);
-						if(value is FormFile)
+						httpRequest.Headers.Add("Accept", "*/*");
+					}
+					else
+					{
+						httpRequest.Headers.Add("Accept", "application/json");
+					}
+					httpRequest.RequestUri = new Uri(request.URL);
+
+					if (request.ContentType == ContentType.MultipartFormData)
+					{
+						var content = new MultipartFormDataContent();
+
+						foreach (var prop in request.Data.GetType().GetProperties())
 						{
-							var file = (FormFile)value;
-							if(file is not null)
+							var value = prop.GetValue(request.Data);
+							if (value is FormFile)
 							{
-								content.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
+								var file = (FormFile)value;
+								if (file is not null)
+								{
+									content.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
+								}
+							}
+							else
+							{
+								content.Add(new StringContent(Convert.ToString(value)), prop.Name);
 							}
 						}
-						else
+
+						httpRequest.Content = content;
+					}
+					else
+					{
+						if (request.Data is not null)
 						{
-							content.Add(new StringContent(Convert.ToString(value)), prop.Name);
+							httpRequest.Content = new StringContent(JsonConvert.SerializeObject(request.Data), Encoding.UTF8, "application/json");
 						}
 					}
 
-					httpRequest.Content = content;
-				}
-				else
-				{
-					if(request.Data is not null)
+					switch (request.RequestType)
 					{
-						httpRequest.Content = new StringContent(JsonConvert.SerializeObject(request.Data), Encoding.UTF8, "application/json");
+						case RequestType.GET:
+							httpRequest.Method = HttpMethod.Get;
+							break;
+						case RequestType.POST:
+							httpRequest.Method = HttpMethod.Post;
+							break;
+						case RequestType.PUT:
+							httpRequest.Method = HttpMethod.Put;
+							break;
+						case RequestType.PATCH:
+							httpRequest.Method = HttpMethod.Patch;
+							break;
+						case RequestType.DELETE:
+							httpRequest.Method = HttpMethod.Delete;
+							break;
 					}
-				}
 
-				switch (request.RequestType)
-				{
-					case RequestType.GET:
-						httpRequest.Method = HttpMethod.Get;
-						break;
-					case RequestType.POST:
-						httpRequest.Method = HttpMethod.Post;
-						break;
-					case RequestType.PUT:
-						httpRequest.Method = HttpMethod.Put;
-						break;
-					case RequestType.PATCH:
-						httpRequest.Method = HttpMethod.Patch;
-						break;
-					case RequestType.DELETE:
-						httpRequest.Method = HttpMethod.Delete;
-						break;
-				}
+					return httpRequest;
+				};
 
 				var token = _tokenProvider.GetToken();
 				if (withBearer && token is not null && !string.IsNullOrWhiteSpace(token.AccessToken))
@@ -93,7 +98,7 @@ namespace MagicVilla.Web.Services
 					client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
 				}
 
-				var apiResponse = await client.SendAsync(httpRequest);
+				var apiResponse = await client.SendAsync(httpRequestMessageFactory());
 				var apiContent = await apiResponse.Content.ReadAsStringAsync();
 
 				try
