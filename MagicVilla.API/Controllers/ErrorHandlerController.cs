c7b3211e0769f8ce2bfd526095a7995a93626c73
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-04 02:11:40 +0500

    ProcessError implemented in ErrorHandlerController.

---
+++ MagicVilla.API/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers
+{
+	[Route("api/ErrorHandler")]
+	[ApiController]
+	[ApiVersionNeutral]
+	public class ErrorHandlerController : ControllerBase
+	{
+		[Route("ProcessError")]
+		public IActionResult ProcessError()
+		{
+			return Problem();
+		}
+	}
+}
