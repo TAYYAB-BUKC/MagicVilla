4c2f3c0f6308c58e8fa847f7be206ff0ba506958
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-04 03:15:36 +0500

    CustomExceptionMiddleware implemented.

---
+++ MagicVilla.API/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+
+namespace MagicVilla.API.Middlewares
+{
+	public class CustomExceptionMiddleware
+	{
+		private RequestDelegate _requestDelegate;
+
+		public CustomExceptionMiddleware(RequestDelegate requestDelegate)
+		{
+			_requestDelegate = requestDelegate;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			try
+			{
+				await _requestDelegate(context);
+			}
+			catch (Exception exception)
+			{
+				await ProcessError(context, exception);
+			}
+		}
+
+		private async Task ProcessError(HttpContext context, Exception exception)
+		{
+			context.Response.StatusCode = 500;
+			context.Response.ContentType = "application/json";
+			if (exception is BadImageFormatException)
+			{
+				await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+				{
+					From = "Program.cs",
+					Title = "BadImageException",
+					StatusCode = 550,
+					ErrorMessage = exception.Message,
+					StackTrace = exception.StackTrace
+				}));
+			}
+			else
+			{
+				await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+				{
+					From = "Program.cs",
+					StatusCode = 500,
+					ErrorMessage = exception.Message,
+					StackTrace = exception.StackTrace,
+				}));
+			}
+		}
+	}
+}
