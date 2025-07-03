bae2c8d94af121d2401e46aec33cfee198a09cce
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-04 02:32:04 +0500

    CustomExceptionFilter implementation added.

---
+++ MagicVilla.API/ExceptionFilters/CustomExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MagicVilla.API.ExceptionFilters
+{
+	public class CustomExceptionFilter : IActionFilter
+	{
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			if(context.Exception is FileNotFoundException fileNotFoundException)
+			{
+				context.Result = new ObjectResult($"File not found exception handle by {nameof(CustomExceptionFilter)}")
+				{
+					StatusCode = 500
+				};
+			}
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
